ed1829b6b1435186ae3cf0a4f2c9bd98f0970512
Anesti Andoni <myemail@gmail.comgit config --global user.email>
2020-11-03 14:28:11 +0100

    including ngrx in the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './core/main-page/main-page.component';
+import { ForcastListComponent } from './core/forcast-list/forcast-list.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {
+    path: '', component: MainPageComponent
+  },
+  {
+    path: 'forecast/:zipcode', component: ForcastListComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,33 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './core/main-page/main-page.component';
 import { ForcastListComponent } from './core/forcast-list/forcast-list.component';
 import { CurrentConditionsComponent } from './core/current-conditions/current-conditions.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http";
+import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     ForcastListComponent,
-    CurrentConditionsComponent
+    CurrentConditionsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot({stateKey: 'router'})
   ],
   providers: [],
   bootstrap: [AppComponent]
